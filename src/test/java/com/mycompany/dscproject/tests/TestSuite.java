51be61a3c5bc48780e9caed7d816014b63d9b8aa
Lucas Gomes de Oliveira <lucas@academico.local>
2022-11-04 13:16:36 -0300

    test: mount the tests suite skeleton

---
+++ src/test/java/com/mycompany/dscproject/tests/TestSuite.java
@@ -0,0 +1,12 @@
+package com.mycompany.dscproject.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    com.mycompany.dscproject.tests.userTest.class
+})
+public class TestSuite {
+    
+}
