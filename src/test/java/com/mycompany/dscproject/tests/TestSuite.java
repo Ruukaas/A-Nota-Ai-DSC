0429cc41b6cdd64737d15a78fefeed192d88c7c9
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2022-11-07 15:22:51 -0300

    test: register tests classes at 'TestSuite.java'

--- src/test/java/com/mycompany/dscproject/tests/TestSuite.java
+++ src/test/java/com/mycompany/dscproject/tests/TestSuite.java
@@ -5,8 +5,11 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+    com.mycompany.dscproject.tests.ItemTest.class,
+    com.mycompany.dscproject.tests.LojaTestes.class,
+    com.mycompany.dscproject.tests.NotaFiscalTest.class,
+    com.mycompany.dscproject.tests.PrecoTest.class,
+    com.mycompany.dscproject.tests.ProdutoTest.class,
     com.mycompany.dscproject.tests.userTest.class
 })
-public class TestSuite {
-    
-}
+public class TestSuite {}
