bf7aea9e9579fa39109e9e9d385ef388c714e742
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-02-03 14:24:22 -0300

    feat: add more 5 tests to project

--- src/test/java/com/mycompany/dscproject/tests/PrecoJPQL.java
+++ src/test/java/com/mycompany/dscproject/tests/PrecoJPQL.java
@@ -1,7 +1,9 @@
 package com.mycompany.dscproject.tests;
 
 import com.mycompany.dscproject.model.Preco;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
+import java.text.DecimalFormat;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -16,5 +18,46 @@ public class PrecoJPQL extends BaseTests {
         List<Preco> precos = query.getResultList();
         
         assertEquals(3, precos.size());
+    }
+    
+    @Test
+    public void menorPrecoRegistrado() {
+        logger.info("Executando menorPrecoRegistrado()");
+        
+        Query query = em.createQuery("SELECT MIN(p.valor) FROM Preco p");
+        Double menorValor =(Double) query.getSingleResult();
+        
+        assertEquals(Double.valueOf(4.99), menorValor);
+    }
+    
+    @Test
+    public void maiorPrecoRegistrado() {
+        logger.info("Executando maiorPrecoRegistrado()");
+        
+        Query query = em.createQuery("SELECT MAX(p.valor) FROM Preco p");
+        Double maiorValor =(Double) query.getSingleResult();
+        
+        assertEquals(Double.valueOf(134.99), maiorValor);
+    }
+    
+    @Test
+    public void mediaPrecosRegistrados() {
+        logger.info("Executando mediaPrecosRegistrado()");
+        
+        Query query = em.createQuery("SELECT AVG(p.valor) FROM Preco p");
+        double mediaPreco =(double) query.getSingleResult();
+        
+        DecimalFormat df = new DecimalFormat("#.##");       
+        assertEquals(df.format(55.19), df.format(mediaPreco));
+    }
+    
+    @Test
+    public void quantidadeDePrecosRegistrados() {
+        logger.info("Executando quantidadeDePrecosRegistrados()");
+        
+        Query query = em.createQuery("SELECT COUNT(p) FROM Preco p");
+        Long quantidadePrecosRegistrados =(Long) query.getSingleResult();
+        
+        assertEquals(Long.valueOf(5), quantidadePrecosRegistrados);
     }
 }
--- src/test/java/com/mycompany/dscproject/tests/ProdutoJPQL.java
+++ src/test/java/com/mycompany/dscproject/tests/ProdutoJPQL.java
@@ -5,6 +5,7 @@ import jakarta.persistence.TypedQuery;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import com.mycompany.dscproject.model.Preco;
 
 public class ProdutoJPQL extends BaseTests{
     @Test
@@ -21,5 +22,16 @@ public class ProdutoJPQL extends BaseTests{
         assertEquals("Garrafa Folgosa", produtos.get(3).getNome());
         assertEquals("Barra de chocolate", produtos.get(4).getNome());
     }
-     
+    
+    @Test
+    public void historicoDePrecosDeTrident() {
+        logger.info("Executando historicoDePrecosDeTrident()");
+        
+        TypedQuery<Preco> query = em.createQuery("SELECT pp FROM Produto p JOIN p.historicoDeValores pp WHERE p.codigo = 3", Preco.class);
+        List<Preco> precosTrident = query.getResultList();
+        
+        assertEquals(2, precosTrident.size());
+        assertEquals(Double.valueOf(4.99), precosTrident.get(0).getValor());
+        assertEquals(Double.valueOf(5.99), precosTrident.get(1).getValor());
+    }
 }
